309d53a96b1822d7cd4d974e9e87189bb8b0632a
rog07phone <rog07phone@gmail.com>
2025-06-24 12:02:34 +0800

    電磁閥arduino

---
+++ Arduino2ARM/arduino_python/sol.vavle/sol.vavle.ino
@@ -0,0 +1,32 @@
+const int VALVE_ENB = 2;     // 電磁閥 ENB (PWM)
+const int VALVE_IN4 = 3;     // 電磁閥 IN4
+const int VALVE_IN3 = 4;     // 電磁閥 IN3
+const int VALVE_PWM = 255;
+
+void setup() {
+  pinMode(VALVE_IN3, OUTPUT);   
+  pinMode(VALVE_IN4, OUTPUT);   
+  pinMode(VALVE_ENB, OUTPUT);
+  delay(1000);
+}
+
+// 電磁閥打開
+void valveOpenExhaust() {
+  digitalWrite(VALVE_IN3, HIGH);
+  digitalWrite(VALVE_IN4, LOW);
+  analogWrite(VALVE_ENB, VALVE_PWM);
+}
+
+// 電磁閥封閉
+void valveCloseExhaust() {
+  digitalWrite(VALVE_IN3, LOW);
+  digitalWrite(VALVE_IN4, LOW);
+  analogWrite(VALVE_ENB, 0);
+}
+
+void loop() {
+  valveOpenExhaust();
+  delay(100);
+  valveCloseExhaust();
+  delay(100);
+}
